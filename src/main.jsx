7ca1f653175902ecd3f9c91c7eaca0a142f189f1
sebastianbarrerah <sebasbh2008@hotmail.com>
2023-07-20 10:44:27 -0500

    slider

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Principal from './components/principal/principal'
+import Slider from './components/slider/slider'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-  <Principal />
+  <Slider />
   </>
   
 )
